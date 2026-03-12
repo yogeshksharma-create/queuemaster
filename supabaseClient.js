0838725ae660228dd4b146539c884734c1d966a3
yogeshksharma-create <yogeshksharma@gmail.com>
2026-03-12 16:21:06 -0400

    Change Supabase anonymous key
    Updated the Supabase anonymous key to a new value.

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://dwgnjnljpractxstijbx.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_oNiHxf9sCWmw0Q5pAVpi0A_m7yUKdi_";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3Z25qbmxqcHJhY3R4c3RpamJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyNzg2MzUsImV4cCI6MjA4ODg1NDYzNX0.i0NA3K9GOIFl1zLlzisoesp9W9oGl_AMOWU0rIGTUJQ";
 
 if (!window.supabase) {
   console.error("Supabase JS library did not load before supabaseClient.js");
